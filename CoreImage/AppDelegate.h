4cb258c5a3ab07e2d80ea09d918d27cc238f3fe8
ok17175 <ok17175@gmail.com>
2013-10-01 13:25:55 +0800

    Initial Commit

---
+++ CoreImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreImage
+//
+//  Created by 李深龙 on 13-10-1.
+//  Copyright (c) 2013年 李深龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreImage
+//
+//  Created by 李深龙 on 13-10-1.
+//  Copyright (c) 2013年 李深龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
